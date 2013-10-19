e5235bf4c13289a4510b793f5f5dbfba40404b33
Atamert Ölçgen <muhuk@muhuk.com>
2013-10-19 22:38:42 +0800

    Implemented circumcircle function.

--- src/triangulate/core.clj
+++ src/triangulate/core.clj
@@ -1,6 +1,36 @@
 (ns triangulate.core)
 
 
+(declare distance sides)
+(defn circumcircle [a b c]
+  (let [[Ax Ay] a
+        [Bx By] b
+        [Cx Cy] c
+        dA (+ (* Ax Ax) (* Ay Ay))
+        dB (+ (* Bx Bx) (* By By))
+        dC (+ (* Cx Cx) (* Cy Cy))
+        [AB BC AC] (sides a b c)
+        origin-denom (* 2
+                        (+ (* Ax (- Cy By))
+                           (* Bx (- Ay Cy))
+                           (* Cx (- By Ay))))
+        Ox (/ (+ (* dA (- Cy By))
+                 (* dB (- Ay Cy))
+                 (* dC (- By Ay)))
+              origin-denom)
+        Oy (/ (+ (* dA (- Cx Bx))
+                 (* dB (- Ax Cx))
+                 (* dC (- Bx Ax)))
+              (* -1 origin-denom))
+        center [Ox Oy]
+        radius (/ (* AB BC AC)
+                  (Math/sqrt (* (+ AB BC AC)
+                                (- (+ AB BC) AC)
+                                (- (+ BC AC) AB)
+                                (- (+ AB AC) BC))))]
+    [center radius]))
+
+
 (defn distance
   "Calculate distance between two points."
   [[x1 y1] [x2 y2]]
--- test/triangulate/core_test.clj
+++ test/triangulate/core_test.clj
@@ -3,13 +3,37 @@
             [triangulate.core :refer :all]))
 
 
+(defn- fuzzy= [tolerance x y]
+  (let [diff (Math/abs (- x y))]
+    (< diff tolerance)))
+
+
+(deftest test-circumcircle
+  (testing "Circumcircle on (1 1) with radius 1."
+    (let [points [[0.070589167 0.6309532502]
+                  [1.237614345 1.9713595746]
+                  [1.0196496048 0.0001930721]]
+          [[Ox Oy] radius] (apply circumcircle points)]
+    (fuzzy= 0.001 Ox 1.0)
+    (fuzzy= 0.001 Oy 1.0)
+    (fuzzy= 0.001 radius 1.0)))
+  (testing "Circumcircle on (300 200) with radius 150."
+    (let [points [[343.3170006374 56.390677685]
+                  [156.3872340216 243.3055821811]
+                  [445.6158448089 236.0003575065]]
+          [[Ox Oy] radius] (apply circumcircle points)]
+    (fuzzy= 0.001 Ox 300.0)
+    (fuzzy= 0.001 Oy 200.0)
+    (fuzzy= 0.001 radius 150.0))))
+
+
 (deftest test-distance
   (testing "Two points in exact same locations have zero distance."
     (let [point [22.797 3.001]]
-      (is (distance point point) 0)))
+      (is (fuzzy= 0.001 (distance point point) 0.0))))
   (testing "Two points on the same axis."
-    (is (distance [150 10] [150 90]) 80)
-    (is (distance [10 40] [60 40]) 50)))
+    (is (fuzzy= 0.001 (distance [150 10] [150 90]) 80.0))
+    (is (fuzzy= 0.001 (distance [10 40] [60 40]) 50.0))))
 
 
 (deftest test-sides
