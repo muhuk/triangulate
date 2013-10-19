669cbf4976d6feb49e694b4700b1bc0ce3b46eea
Atamert Ölçgen <muhuk@muhuk.com>
2013-10-19 21:29:10 +0800

    Added distance & sides functions.

--- src/triangulate/core.clj
+++ src/triangulate/core.clj
@@ -1,6 +1,16 @@
 (ns triangulate.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn distance
+  "Calculate distance between two points."
+  [[x1 y1] [x2 y2]]
+  (Math/sqrt (+ (Math/pow (- x2 x1) 2)
+                (Math/pow (- y2 y1) 2))))
+
+
+(defn sides
+  "Calculate the lengths of the sides of a triangle."
+  [a b c]
+  (vector (distance a b)
+          (distance b c)
+          (distance a c)))
--- test/triangulate/core_test.clj
+++ test/triangulate/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [triangulate.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest test-distance
+  (testing "Two points in exact same locations have zero distance."
+    (let [point [22.797 3.001]]
+      (is (distance point point) 0)))
+  (testing "Two points on the same axis."
+    (is (distance [150 10] [150 90]) 80)
+    (is (distance [10 40] [60 40]) 50)))
+
+
+(deftest test-sides
+  (testing "Sides of a right triangle."
+    (is (sides [5 5] [9 5] [9 8]) [4 3 5])))
