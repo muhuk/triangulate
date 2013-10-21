d2d7bae4bfd2abaab5a67ece9e0bd563dcc10013
Atamert Ölçgen <muhuk@muhuk.com>
2013-10-21 23:02:51 +0800

    Implemented triangulate function.

--- src/triangulate/core.clj
+++ src/triangulate/core.clj
@@ -41,8 +41,22 @@
                 (Math/pow (- y2 y1) 2))))
 
 
+(defn find-edges
+  "TODO"
+  [triangles]
+  (let [edge-fn (fn [triangle a b] (vec (sort (vector (a triangle)
+                                                      (b triangle)))))
+        all-edges (vec (map #(vector (edge-fn % :a :b)
+                                (edge-fn % :b :c)
+                                (edge-fn % :a :c)) triangles))
+        flattened-edges (reduce concat [] all-edges)]
+    (vec (map first
+              (filter #(= (second %) 1)
+                      (frequencies flattened-edges))))))
+
+
 (defn make-triangle
-  ""
+  "TODO"
   [points ^long a ^long b ^long c]
   (let [A (nth points a)
         B (nth points b)
@@ -57,3 +71,65 @@
   (vector (distance a b)
           (distance b c)
           (distance a c)))
+
+
+(defn point-in-circle?
+  "Test whether the point is inside the circle."
+  [point center radius]
+    (< (distance point center) radius))
+
+
+(declare point-in-circle?)
+(defn push-vertex
+  "TODO"
+  [triangles points i]
+  (let [point (nth points i)
+        grouper (fn [triangle]
+                  (if (point-in-circle? point
+                                        (:circumcenter triangle)
+                                        (:radius triangle))
+                      :inside
+                      :outside))
+        {:keys [inside outside]} (group-by grouper triangles)
+        edges (find-edges inside)
+        new-triangles (map #(make-triangle points (first %) (second %) i) edges)]
+    (concat outside new-triangles)))
+
+
+(defn push-vertices
+  "TODO"
+ [triangles points n]
+ (loop [tris triangles i 0]
+    (if (< i n)
+        (recur (push-vertex tris points i) (inc i))
+        tris)))
+
+
+(defn triangulate
+  "TODO"
+  [points]
+  (let [xs (map first points)
+        ys (map second points)
+        min-x (apply min xs)
+        max-x (apply max xs)
+        min-y (apply min ys)
+        max-y (apply max ys)
+        n (count points)
+        margin (/ (max (- max-x min-x) (- max-y min-y)) 10)
+        temp-points [[(- min-x margin) (- min-y margin)]
+                     [(+ max-x margin) (- min-y margin)]
+                     [(- min-x margin) (+ max-y margin)]
+                     [(+ max-x margin) (+ max-y margin)]]
+        all-points (concat points temp-points)
+        super-triangles [(make-triangle all-points
+                                        n
+                                        (+ n 1)
+                                        (+ n 2))
+                         (make-triangle all-points
+                                        (+ n 1)
+                                        (+ n 2)
+                                        (+ n 3))]]
+    (filter #(and (< (:a %) n)
+                  (< (:b %) n)
+                  (< (:c %) n))
+            (push-vertices super-triangles all-points n))))
