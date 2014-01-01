757e5d148ea6a4c4d8621fa5b1736f3edc9f5c1a
Atamert Ölçgen <muhuk@muhuk.com>
2014-01-01 19:18:43 +0800

    Added documentation.

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :aliases {"benchmark" ["trampoline" "run" "-m" "triangulate.benchmark"]}
   :jar-exclusions [#"^triangulate/benchmark\.clj$"]
-  :plugins  [[lein-cloverage "1.0.2"]]
+  :plugins  [[codox "0.6.6"]
+             [lein-cloverage "1.0.2"]]
   :profiles {:dev {:dependencies [[criterium "0.4.2"]]}})
