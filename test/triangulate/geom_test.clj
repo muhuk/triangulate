ebc85bc5139cead408848f026918438e1ea3694e
Atamert Ölçgen <muhuk@muhuk.com>
2014-01-01 22:09:22 +0800

    Improved comparison function fuzzy=.

--- test/triangulate/geom_test.clj
+++ test/triangulate/geom_test.clj
@@ -6,9 +6,12 @@
             [triangulate.model :refer [->Point]]))
 
 
-(defn- fuzzy= [tolerance x y]
-  (let [diff (Math/abs (- x y))]
-    (< diff tolerance)))
+(defn- fuzzy=
+  ([a b]
+    (fuzzy= a b 0.001))
+  ([a b tolerance]
+    (let [diff (Math/abs (- a b))]
+      (< diff tolerance))))
 
 
 (deftest test-circumcircle
@@ -19,9 +22,9 @@
           [origin radius] (apply circumcircle points)
            Ox (:x origin)
            Oy (:y origin)]
-    (fuzzy= 0.001 Ox 1.0)
-    (fuzzy= 0.001 Oy 1.0)
-    (fuzzy= 0.001 radius 1.0)))
+    (fuzzy= Ox 1.0)
+    (fuzzy= Oy 1.0)
+    (fuzzy= radius 1.0)))
   (testing "Circumcircle on (300 200) with radius 150."
     (let [points [(->Point 343.3170006374 56.390677685)
                   (->Point 156.3872340216 243.3055821811)
@@ -29,18 +32,18 @@
           [origin radius] (apply circumcircle points)
            Ox (:x origin)
            Oy (:y origin)]
-    (fuzzy= 0.001 Ox 300.0)
-    (fuzzy= 0.001 Oy 200.0)
-    (fuzzy= 0.001 radius 150.0))))
+    (fuzzy= Ox 300.0)
+    (fuzzy= Oy 200.0)
+    (fuzzy= radius 150.0))))
 
 
 (deftest test-distance
   (testing "Two points in exact same locations have zero distance."
     (let [point (->Point 22.797 3.001)]
-      (is (fuzzy= 0.001 (distance point point) 0.0))))
+      (is (fuzzy= (distance point point) 0.0))))
   (testing "Two points on the same axis."
-    (is (fuzzy= 0.001 (distance (->Point 150 10) (->Point 150 90)) 80.0))
-    (is (fuzzy= 0.001 (distance (->Point 10 40) (->Point 60 40)) 50.0))))
+    (is (fuzzy= (distance (->Point 150 10) (->Point 150 90)) 80.0))
+    (is (fuzzy= (distance (->Point 10 40) (->Point 60 40)) 50.0))))
 
 
 (deftest test-sides
